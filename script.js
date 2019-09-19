73ca91b5d97568a74cf15f8dbc664fc3bd1d3643
Kevin Ngo <ngokevin39@gmail.com>
2019-09-18 21:20:25 -0400

    implemented enemy spawning, collision detection

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var CANVAS_WIDTH = 480;
-var CANVAS_HEIGHT = 320;
+var CANVAS_WIDTH = 720;
+var CANVAS_HEIGHT = 480;
 var PLAYER_MOVESPEED = 15;
+var DIFFICULTY = prompt("ENTER DIFFICULTY",1);
+
+var enemies = [];
+var FPS = 30;
 var canvasElement = document.createElement("canvas");
 canvasElement.setAttribute("width", CANVAS_WIDTH);
 canvasElement.setAttribute("height", CANVAS_HEIGHT);
@@ -9,6 +13,43 @@ canvasElement.setAttribute("style", "border:solid 1px;");
 
 var canvas = canvasElement.getContext("2d");
 document.body.appendChild(canvasElement);
+
+
+class Bullet {
+  constructor(x, y, direction) {
+    this.color = "#AAA";
+    this.x = x;
+    this.y = y;
+    this.width = 15;
+    this.height = 15;
+    this.direction = direction;
+    this.isActive = true;
+  }
+
+  update() {
+    if (this.direction === "N") {
+      this.y -= 10;
+    } else if (this.direction === "E") {
+      this.x += 10;
+    } else if (this.direction === "S") {
+      this.y += 10;
+    } else if (this.direction === "W") {
+      this.x -= 10;
+    }
+  }
+
+  draw() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, 15, 15);
+  }
+
+  wallCollision() {
+    if (this.y <= 0 || this.y >= CANVAS_HEIGHT || this.x <= 0 || this.x >= CANVAS_WIDTH) {
+      this.isActive = false;
+    }
+   
+  }
+};
 
 
 class Player {
@@ -22,105 +63,139 @@ class Player {
     this.bullets = [];
   }
 
+  update = (keyCode) => {
+    if (keyCode === 37) {
+      this.x -= PLAYER_MOVESPEED;
+      this.direction = "W";
+    }
+    //up
+    else if (keyCode === 38) {
+      this.y -= PLAYER_MOVESPEED;
+      this.direction = "N";
+    }
+    //right
+    else if (keyCode === 39) {
+      this.x += PLAYER_MOVESPEED;
+      this.direction = "E";
+    }
+    //down
+    else if (keyCode === 40) {
+      this.y += PLAYER_MOVESPEED;
+      this.direction = "S";
+    }
+    //space
+    else if (keyCode === 32) {
+      this.bullets.push(new Bullet(this.x, this.y, this.direction));
+    }
+
+    this.x = clampSpriteToWalls(this.x, 0, CANVAS_WIDTH - this.width);
+    this.y = clampSpriteToWalls(this.y, 0, CANVAS_HEIGHT - this.height);
+  }
+
   draw = () => {
     canvas.fillStyle = this.color;
     canvas.fillRect(this.x, this.y, this.width, this.height);
-    if (this.bullets.length > 0) {
-      for (var index = 0 ; index < this.bullets.length; ++index) {
-        this.bullets[index].update();
+  }
 
-        if (this.bullets[index].collision()) {
-          this.bullets[index].draw();
-        }else {
-          this.bullets = this.bullets.filter((value) => value.collision());
-        }
+
+};
+
+class Enemy {
+  constructor(x, y) {
+    this.color = "#fc4503";
+    this.x = x;
+    this.y = y;
+    this.width = 32;
+    this.height = 32;
+    this.direction = "N";
+    this.bullets = [];
+    this.isActive = true;
+  }
+
+  update = () => {
+    //decide to move x or y
+    //move x
+    if (Math.floor(Math.random()*2) === 1) {
+      if (this.x < player.x) {
+        this.x += 5;
+      } else {
+        this.x -= 5;
+      }
+    //move y
+    } else {
+      if (this.y < player.y) {
+        this.y += 5;
+      } else {
+        this.y -= 5;
       }
     }
   }
 
-
-};
-
-class Bullet {
-  constructor(x, y, direction) {
-    this.color = "#AAA";
-    this.x = x;
-    this.y = y;
-    this.direction = direction;
-    // this.collisio = false;
-  }
-
-  update() {
-    this.x += 0;
-    this.y -= 10;
-  }
-
-  draw() {
+  draw = () => {
     canvas.fillStyle = this.color;
-    canvas.fillRect(this.x, this.y, 15, 15);
+    canvas.fillRect(this.x, this.y, this.width, this.height);
   }
-
-  collision() {
-    if (this.direction === "N" && this.y <= 0) {
-      return false;
-    }
-    return true;
-  }
-
 };
 
-// function collisonDetection(obj1, obj2) {...}
+var clampSpriteToWalls = (position, min, max) => position <= min ? min : position >= max ? max : position;
 
-function collisionDetectionWall(obj1) {
-  if (obj1.x <= 0 || obj1.y <= 0) {
-    return true;
-  }
-  return false;
+var drawClearBoard = () => canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
+var objCollision = (a, b) => a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
+
+var randomEnemyPosition = () => new Enemy(Math.floor(Math.random() * Math.floor(CANVAS_WIDTH - 32)), Math.floor(Math.random() * Math.floor(CANVAS_HEIGHT - 32)));
+
+
+function handleCollisions() {
+  player.bullets.forEach(function(bullet) {
+    bullet.wallCollision();
+    enemies.forEach(function(enemy) {
+      if (objCollision(bullet, enemy)) {
+        // enemy.explode();
+        enemy.isActive = false;
+        bullet.isActive = false;
+      }
+    });
+  });
+
+  // enemies.forEach(function(enemy) {
+    // if (objCollision(enemy, player)) {
+      // player = null;
+  //     enemy.explode();
+  //     player.explode();
+    // }
+  // });
 }
 
 var player = new Player();
-var FPS = 30;
+
+
+//general update and drawing
 setInterval(function() {
-  draw();
+  //updating
+  player.bullets.forEach(item => item.update());
+
+  //check collision
+  handleCollisions();
+
+  player.bullets = player.bullets.filter((value) => value.isActive);
+  enemies.forEach(item => item.update());
+  enemies = enemies.filter((value) => value.isActive);
+
+  //drawing
+  drawClearBoard();
   player.draw();
+  enemies.forEach(item => item.draw());
+
+  player.bullets.forEach(item => item.draw());
 }, 1000/FPS);
 
-
-
-
-function draw() {
-  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
-
-
-
-
+//spawn rate of enemies
+setInterval(function(){
+  enemies.push(randomEnemyPosition());
+},5000/DIFFICULTY);
 
 
 document.addEventListener("keydown", (event) => {
-//left
-if (event.keyCode === 37) {
-  player.x -= PLAYER_MOVESPEED;
-  player.direction = "W";
-}
-//up
-else if (event.keyCode === 38) {
-  player.y -= PLAYER_MOVESPEED;
-  player.direction = "N";
-}
-//right
-else if (event.keyCode === 39) {
-  player.x += PLAYER_MOVESPEED;
-  player.direction = "E";
-}
-//down
-else if (event.keyCode === 40) {
-  player.y += PLAYER_MOVESPEED;
-  player.direction = "S";
-}
-//space
-else if (event.keyCode === 32) {
-  player.bullets.push(new Bullet(player.x, player.y, "N"));
-}
+  player.update(event.keyCode);
 });
